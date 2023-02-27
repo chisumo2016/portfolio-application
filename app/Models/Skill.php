5207984461162b567c67f81c6e1ffc4e6cb48c1b
bernard chisumo <bchisumo74@gmail.com>
2023-02-27 14:44:54 +0000

    Implemented mass assigment and relationship for both Projet & Skill model

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Skill extends Model
 {
     use HasFactory;
+    protected  $fillable = [
+        'name',
+        'image'
+    ];
+
+    public function projects():HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
